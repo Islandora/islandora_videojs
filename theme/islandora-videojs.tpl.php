c87fe64684719ae81bc0523a4944cfa803b8bc83
Nelson Hart <nelson.hart@gmail.com>
2013-11-20 09:22:29 -0400

    use template file for player

---
+++ theme/islandora-videojs.tpl.php
@@ -0,0 +1,7 @@
+<video id="my_video_1" class="video-js vjs-default-skin" controls
+ preload="auto" width="640" height="264" poster="<?php print $tn; ?>"
+ data-setup="{}">
+  <?php foreach ($sources as $source): ?>
+  <source src="<?php print $source['url']; ?>" type='<?php print $source['mime']; ?>'>
+  <?php endforeach; ?>
+</video>
