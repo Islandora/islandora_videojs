7f1cc4780948925d73e2913b87558cf47d69d871
nruest <ruestn@gmail.com>
2014-02-12 13:35:46 -0500

    better video id

--- theme/islandora-videojs.tpl.php
+++ theme/islandora-videojs.tpl.php
@@ -1,4 +1,4 @@
-<video id="my_video_1" class="video-js vjs-default-skin" controls
+<video id="islandora_videojs" class="video-js vjs-default-skin" controls
  preload="auto" width="640" height="264" poster="<?php print $tn; ?>"
  data-setup="{}">
   <?php foreach ($sources as $source): ?>
